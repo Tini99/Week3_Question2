80f411bd0a02fb4e945b67201814c9b92ea5eabd
Tini Gisa <1814759@nzse.ac.nz>
2021-06-20 17:29:06 +1200

    Add project files.

---
+++ Week3_Question2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Week3_Question2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Declare and initialize array of 10 integers.
+            then print the multiplication table of each element in the array.
+             */
+            int[] array = new int[10];
+            int i;
+
+            for (i = 0; i <= 9; i++)
+            {
+                Console.WriteLine("Enter a multiplication");
+                i = Convert.ToInt32(Console.ReadLine());
+                {
+                    Console.WriteLine(array[9]);
+                }
+            }
+        }
+    }
+}
